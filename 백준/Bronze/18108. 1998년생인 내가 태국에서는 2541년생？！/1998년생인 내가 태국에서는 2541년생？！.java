893cef6037492fa9c31ad1858dcd4169d992930a
ratel.sho <143933603+ratel-sho@users.noreply.github.com>
2023-09-20 11:28:19 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		int year;
+		
+		year = sc.nextInt();			
+	
+		System.out.println(year-543);		
+				
+	}
+}
