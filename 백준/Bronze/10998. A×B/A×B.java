3267386528d15a5fe400749ba3174226d55cf3cf
ratel.sho <143933603+ratel-sho@users.noreply.github.com>
2023-09-20 11:02:32 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		int a,b;
+
+		a = sc.nextInt();
+		b = sc.nextInt();
+
+		System.out.println(a*b);
+	}
+}
