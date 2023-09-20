742a8b0da2e8665e231aef5e49ee027d3a5061af
ratel.sho <143933603+ratel-sho@users.noreply.github.com>
2023-09-20 11:24:57 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		String name="";
+		
+		name = sc.nextLine();			
+	
+		System.out.println(name+"??!");		
+				
+	}
+}
+
