2fb28690b6d6a50d457190286539a28e5b0d57f9
ratel.sho <143933603+ratel-sho@users.noreply.github.com>
2023-10-30 09:42:00 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int Tlong = Integer.parseInt(br.readLine());
+		int Lcount = Tlong/4; 
+		
+		for(int i = 0; i<Lcount; i++) {
+			
+			bw.write("long "); 
+		}
+		
+			bw.write("int" + "\n");
+		
+		bw.flush();
+		bw.close();		
+	}
+}
+
